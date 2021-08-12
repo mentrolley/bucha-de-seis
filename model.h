5fd3745e144ea5f2257fc45193cc078e653ca2e5
gsalesc <88722237+gsalesc@users.noreply.github.com>
2021-08-12 13:31:09 -0300

    Update model.h

--- model.h
+++ model.h
@@ -1,1 +1,15 @@
+typedef struct
+{
+    int p1;
+    int p2;
+    int Vh; //tamanho das dezenas para comparação de tamanho
+} nomePecas; //peça
 
+typedef struct
+{
+    nomePecas pecasPlayers[7]; //7 peças
+} players; //jogadores
+
+nomePecas pecas[28]; //peças totais
+players player1; //jogador 1 com 7 peças
+players player2; //jogador 2 com 7 peças
